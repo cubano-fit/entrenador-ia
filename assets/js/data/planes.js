22bf123b7b6cf4d7504a464f70e3c3c35169ba13
Entrenador personal IA para Cuba <163480369+cubano-fit@users.noreply.github.com>
2026-03-08 05:40:47 -0400

    Delete assets/js directory

--- assets/js/data/planes.js
+++
@@ -1,8 +0,0 @@
-window.planesNutricionales = {
-    'perder peso': [
-        { des: '2 huevos + café + plátano', alm: 'Pollo + ensalada', mer: 'Manzana', cena: 'Pescado + viandas' }
-    ],
-    'ganar músculo': [
-        { des: '3 huevos + avena', alm: 'Arroz + pollo + frijoles', mer: 'Batido', cena: 'Pescado + yuca' }
-    ]
-};
